bbb8d52f21b138a0110a471512495bd0afaaa065
AlekseiMakhov <66823049+AlekseiMakhov@users.noreply.github.com>
2021-01-29 20:37:45 +0300

    installed bcryptjs

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
